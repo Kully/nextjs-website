ded3f5d576ae3007e6b15eea09492c3e98f6645a
Adam Kulidjian <adam.kulidjian@gmail.com>
2024-07-28 10:06:51 -0400

    feat: remove calendly button
    Since the Calendly button does nothing, this commit removes it from the Contact Us section.

--- sections/contact.js
+++ sections/contact.js
@@ -2,7 +2,7 @@ import Section from '../components/section/section'
 import SectionHeading from '../components/section/sectionHeading'
 import SectionContent from '../components/section/sectionContent'
 // import FormContact from '../components/forms/formContact'
-import CalloutCalendly from '../components/callouts/calloutCalendly'
+// import CalloutCalendly from '../components/callouts/calloutCalendly'
 
 const Contact = () => (
   <Section id="contact">
@@ -13,7 +13,6 @@ const Contact = () => (
           <p>
           Let's talk about your project! 🤝<br/><br/>Send us an email at zyphr.form@gmail.com or book a timeslot and let us know what you have in mind.
           </p>
-          <CalloutCalendly url="https://calendly.com/zyphr-form/15min" text="Book a call on Calendly" />
         </SectionContent>
       </div>
 {/*      <div className="col-lg-6 col-md-12 col-sm-12">
